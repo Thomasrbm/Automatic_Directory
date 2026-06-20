d7169c185a3966f95912910efb9606d4fcc48974
Thomasrbm <frozyxyt.76@gmail.com>
2026-06-20 17:45:00 +0200

    feat: script lecture de tous les utilisateurs

---
+++ ReadDataBaseInformation.ps1
@@ -0,0 +1,10 @@
+# ReadDataBaseInformation.ps1 - Recupere les informations de tous les utilisateurs AD
+# Parametres : Filter (optionnel)
+. "$PSScriptRoot\helpers.ps1"
+$Filter = Get-OptionalInput "Attributs a recuperer separes par virgules (vide = tout)" "Filtre"
+if ($Filter) {
+    $Props = $Filter -split "," | ForEach-Object { $_.Trim() }
+    Get-ADUser -Filter * -Properties $Props | Select-Object $Props | Format-Table -AutoSize
+} else {
+    Get-ADUser -Filter * -Properties * | Format-List
+}
