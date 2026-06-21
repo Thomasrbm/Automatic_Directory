4b9cd2f9c6831a91d6371e8c1ffd21870d6d156e
Thomasrbm <frozyxyt.76@gmail.com>
2026-06-21 11:40:00 +0200

    feat: script listage membres d'un groupe

---
+++ ListUserInGroup.ps1
@@ -0,0 +1,8 @@
+# ListUserInGroup.ps1 - Liste tous les membres d'un groupe AD
+# Parametres : GroupName
+. "$PSScriptRoot\helpers.ps1"
+$Group = Get-Input "Nom du groupe" "Groupe"
+Assert-GroupExists $Group
+$Members = Get-ADGroupMember -Identity $Group -Recursive
+if ($Members.Count -eq 0) { Write-Host "Aucun membre dans '$Group'." -ForegroundColor Yellow }
+else { $Members | Select-Object Name, SamAccountName, ObjectClass | Format-Table -AutoSize }
