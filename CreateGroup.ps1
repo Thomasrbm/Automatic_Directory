a1d927324ec9729df4e793f0ede7750c9c6caa0a
Thomasrbm <frozyxyt.76@gmail.com>
2026-06-21 10:05:00 +0200

    feat: script creation groupe de securite

---
+++ CreateGroup.ps1
@@ -0,0 +1,10 @@
+# CreateGroup.ps1 - Cree un nouveau groupe de securite AD
+# Parametres : GroupName, OrganisationUnit, GroupScope, Description
+. "$PSScriptRoot\helpers.ps1"
+Test-Admin
+$Name  = Get-Input "Nom du groupe" "Groupe"
+$OU    = Get-Input "Organizational Unit (ex: OU=Workshop,DC=domolia,DC=local)" "OU"
+$Scope = Get-Input "Etendue (Global, Universal, DomainLocal)" "Scope" "Global"
+$Desc  = Get-OptionalInput "Description du groupe" "Description"
+New-ADGroup -Name $Name -GroupScope $Scope -GroupCategory Security -Path $OU -Description $Desc
+Write-Host "Groupe '$Name' cree." -ForegroundColor Green
