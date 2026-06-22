bfa3ba83a56f61e8128e8170d4b7450ba11935eb
Thomasrbm <frozyxyt.76@gmail.com>
2026-06-22 09:30:00 +0200

    feat: script ajout utilisateur dans un groupe

---
+++ AddUserToGroup.ps1
@@ -0,0 +1,11 @@
+# AddUserToGroup.ps1 - Ajoute un utilisateur a un groupe AD
+# Bloque si l'utilisateur est inconnu
+# Parametres : UserName, GroupName
+. "$PSScriptRoot\helpers.ps1"
+Test-Admin
+$User  = Get-Input "Nom du compte utilisateur a ajouter" "Utilisateur"
+Assert-UserExists $User
+$Group = Get-Input "Nom du groupe" "Groupe"
+Assert-GroupExists $Group
+Add-ADGroupMember -Identity $Group -Members $User
+Write-Host "'$User' ajoute au groupe '$Group'." -ForegroundColor Green
